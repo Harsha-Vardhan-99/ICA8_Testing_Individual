bf09e0838d4d63ab3489db9a72c1d85c02da291e
mohana0103 <harshavardhanbodepudi1999@gmail.com>
2022-10-27 22:27:17 -0700

    Added Test Case 4

--- src/test/java/org/example/UrinalsTest.java
+++ src/test/java/org/example/UrinalsTest.java
@@ -7,18 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class UrinalsTest {
     @Test
     public void TestCase1() {
-        System.out.println("======Harshavardhan Bodepudi======Test One Executed====");
+        System.out.println("======Harshavardhan Bodepudi======Test Case One Executed====");
         assertEquals(1,Urinals.getUrinals("100001"));
     }
     @Test
     public void TestCase2() {
-        System.out.println("======Harshavardhan Bodepudi======Test Two Executed====");
+        System.out.println("======Harshavardhan Bodepudi======Test Case Two Executed====");
         assertEquals(0,Urinals.getUrinals("11"));
     }
     @Test
     public void TestCase3() {
-        System.out.println("======Harshavardhan Bodepudi======Test Three Executed====");
-        assertEquals(1,Urinals.getUrinals("000"));
+        System.out.println("======Harshavardhan Bodepudi======Test Case Three Executed====");
+        assertEquals(2,Urinals.getUrinals("000"));
+    }
+    @Test
+    public void TestCase4() {
+        System.out.println("======Harshavardhan Bodepudi======Test Case Four Executed====");
+        assertEquals(0,Urinals.getUrinals("0"));
     }
 
 }
