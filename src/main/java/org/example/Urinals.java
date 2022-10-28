bbffe83cdfdbab04f00dcf112d745b95877ef573
mohana0103 <harshavardhanbodepudi1999@gmail.com>
2022-10-27 22:04:13 -0700

    Implemented file output feature

--- src/main/java/org/example/Urinals.java
+++ src/main/java/org/example/Urinals.java
@@ -1,5 +1,8 @@
 package org.example;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class Urinals {
@@ -9,9 +12,9 @@ public class Urinals {
             Scanner scn = new Scanner(System.in);
             System.out.println("Press 1 to enter input string through keyboard or 2 to read input from file");
             inputChoice = scn.nextInt();
-            System.out.println("Enter your strings:");
             if (inputChoice == 1) {
                 Scanner scn2 = new Scanner(System.in);
+                System.out.println("Enter your strings:");
                 while (scn2.hasNext()) {
                     String inputString = scn2.nextLine();
                     if (!inputString.equals("-1")) {
@@ -23,17 +26,42 @@ public class Urinals {
                     } else System.exit(2);
                 }
             } else if (inputChoice == 2) {
+                String[] fileNames = new String[100];
+                for(int i=0;i<100;i++){
+                    if(i==0){
+                        fileNames[0] = "rule.txt";
+                    }
+                    else{
+                        fileNames[i] = "rule"+ i +".txt";
+                    }
+                }
                 File inputFile = new File("src/main/java/org/example/urinal.dat");
                 FileReader inputDetails = new FileReader(inputFile);
                 BufferedReader inputBuffer = new BufferedReader(inputDetails);
                 String currentLine;
                 boolean flag = false;
-                while((currentLine = inputBuffer.readLine())!=null){
-                    if(isValidString(currentLine)){
-                        System.out.println(getUrinals(currentLine));
+                String path  = "src/main/java/org/example/";
+                for(int i=0;i<100;i++){
+                    File f = new File(path+fileNames[i]);
+                    if(f.exists() && !f.isDirectory()) {
+                        continue;
                     }
                     else{
-                        System.out.println("-1");
+                        path = path + fileNames[i];
+                        break;
+                    }
+                }
+                File myObj = new File(path);
+                myObj.createNewFile();
+                while((currentLine = inputBuffer.readLine())!=null){
+                    String output = "";
+                    if(isValidString(currentLine)){
+                        output = String.valueOf(getUrinals(currentLine));
+                        Files.write(Paths.get(path), output.getBytes(), StandardOpenOption.APPEND);
+                    }
+                    else{
+                        output = "-1";
+                        Files.write(Paths.get(path), output.getBytes(), StandardOpenOption.APPEND);
                     }
                 };
             }
